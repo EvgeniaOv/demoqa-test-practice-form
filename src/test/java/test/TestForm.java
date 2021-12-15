afd98673368e043e10f8c2589c4fb281101feea3
EvgeniaOv <gralph@rambler.ru>
2021-12-15 20:49:00 +0300

    add comits

--- src/test/java/test/TestForm.java
+++ src/test/java/test/TestForm.java
@@ -9,12 +9,19 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestForm {
     @Test
     void fillFromTest() {
+       //открываем страницу с формой в браузере
         open("https://demoqa.com/automation-practice-form");
+        //заполняем текстовое поле Имя
         $("[id=firstName]").setValue("Евгения");
+        //заполняем текстовое поле фамилия
         $("[id=lastName]").setValue("Овечкина");
+        //заполняем текстовое поле эл.адреса
         $("[id=userEmail]").setValue("gralph@rambler.ru");
+        //кликаем на радоио-батн
         $("[for='gender-radio-2']").click();
+        //заполняем текстовое поле телефон
         $("[id=userNumber]").setValue("1234567");
+
 
 
 
