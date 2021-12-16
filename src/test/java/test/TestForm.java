32335ca344b326bf8b7a1ac6b052fd16f09cabe6
EvgeniaOv <gralph@rambler.ru>
2021-12-16 14:42:31 +0300

    hobbies

--- src/test/java/test/TestForm.java
+++ src/test/java/test/TestForm.java
@@ -12,22 +12,30 @@ public class TestForm {
        //открываем страницу с формой в браузере
         open("https://demoqa.com/automation-practice-form");
         //заполняем текстовое поле имя
-        $("[id=firstName]").setValue("Evgenia");
+        $("#firstName").setValue("Evgenia");
         //заполняем текстовое поле фамилия
-        $("[id=lastName]").setValue("Ovechkina");
+        $("#lastName").setValue("Ovechkina");
         //заполняем текстовое поле эл.адреса
-        $("[id=userEmail]").setValue("gralph@rambler.ru");
+        $("#userEmail").setValue("gralph@rambler.ru");
         //кликаем на радоио-батн
         $("[for='gender-radio-2']").click();
         //заполняем текстовое поле телефон
-        $("[id=userNumber]").setValue("1234567");
+        $("#userNumber").setValue("1234567");
         //кликаем на поле дата чтобы всплыл календарь
-        $("[id='dateOfBirthInput']").click();
+        $("#dateOfBirthInput").click();
         //выбор года из календаря
         $(".react-datepicker__year-select").selectOptionByValue("1986");
         //выбор месяца из календаря
         $(".react-datepicker__month-select").selectOptionByValue("5");
-        //выбор даты из календаря
-        $("[class*='datepicker__day--006']").click();
+        //выбор даты из календаря. клик на дату
+        $(".react-datepicker__day--006").click();
+        //заполнение поля сабджект тегом. выбор значения. клик для ввода значения
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        //выбор чек-бокса
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-2']").click();
+
+
+
     }
 }
